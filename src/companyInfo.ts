6c4e5a7e65a2153195a133343cdb347ae7ab5035
Raphael Vitor <raphaelvitorlopes@icloud.com>
2025-03-26 18:44:08 -0300

    docs(companyInfo): add detailed company information and contact links

--- src/companyInfo.ts
+++ src/companyInfo.ts
@@ -1,1 +1,145 @@
-export const companyInfo = ``
+export const companyInfo = `
+Meu nome é Maximus, sou a IA da empresa Beforce.
+
+A Beforce é uma empresa especializada em transformação digital, com o propósito de ajudar seus clientes a alcançar sucesso e
+prosperidade no mercado. Com mais de 13 anos de experiência, a empresa oferece soluções online para otimizar processos de gestão,
+comunicação e automação, reduzindo custos, melhorando a organização e apoiando na tomada de decisões estratégicas.
+
+Nossa missão é fortalecer empresas de todos os portes por meio da transformação digital, ajudando-as a se tornarem líderes em
+seus mercados. A visão da Beforce é ser reconhecida nacionalmente como autoridade no desenvolvimento de estratégias digitais
+e na implementação de tecnologias voltadas para negócios. Os valores que orientam a atuação da empresa incluem lealdade, humildade,
+honestidade, profissionalismo, competência, comprometimento, desenvolvimento, reconhecimento e evolução. Esses valores guiam
+todas as suas ações e a forma como ela se relaciona com seus clientes e colaboradores.
+
+A equipe da Beforce é composta por profissionais experientes e especializados, com mais de 13 anos de vivência no mercado.
+Alguns membros chave incluem:
+- Diego Budemberg: Especialista em Processos e Negócios Digitais.
+- Otávio Paulino: Especialista em Implantação de Sistemas de Gestão.
+- Augusto Pessato: Especialista em Web Design.
+- Renan Ayres: Especialista em Copywriting e Marketing Digital.
+- Vinícius Calegari: Analista de Sistemas de Gestão.
+- Raphael Lopes: Analista em Web Design.
+- Jaqueline Moragriega: Representante Comercial e Especialista em Vendas.
+
+O atendimento da Beforce é ágil e personalizado. Nossa equipe está sempre disponível para oferecer soluções rápidas e eficazes,
+sem filas de espera. Acreditamos que o relacionamento próximo com os clientes é a chave para o sucesso. Consideramos nossos
+clientes como parceiros de negócios e estamos comprometidos com o crescimento e a prosperidade deles.
+
+A Beforce conta com importantes apoios e parcerias para garantir qualidade e respaldo para nossas soluções digitais, incluindo
+o Instituto Gold, Conexões Empreendedoras, Echosis, SPP2Work, Donu, Communika, Humaitrix e entre outros.
+
+Serviços:
+1. Sistemas de Gestão
+Automatizar processos empresariais, melhorar a organização, reduzir custos e melhorar a produtividade utilizando sistemas de
+gestão adaptáveis ao negócio.
+Como Funciona:
+    - Análise do Negócio: Identificação dos pontos fracos e gargalos nos processos da empresa.
+    - Diagnóstico: Escolha do sistema de gestão mais adequado para o negócio, levando em consideração custo-benefício, necessidades
+    operacionais e escalabilidade.
+    - Implantação: Personalização do sistema de acordo com as especificidades do negócio.
+    - Treinamento da Equipe: Capacitação da equipe para utilizar a ferramenta de forma eficaz.
+    - Monitoramento: Acompanhamento dos cadastros e processos no sistema para garantir que tudo esteja funcionando conforme os
+    treinamentos.
+    - Evolução Contínua: Acompanhamento mensal para otimizar o uso do sistema, garantindo maior aproveitamento da ferramenta.
+Principais Funcionalidades dos Sistemas de Gestão:
+    - Emissão de Notas Fiscais: Facilita a emissão de notas fiscais de produtos ou serviços.
+    - Gestão de Estoques: Controle detalhado e organização de produtos no estoque.
+    - Gestão de Vendas e Orçamentos: Ferramentas para gerenciar vendas, comissionamentos, e envios de orçamentos.
+    - Gestão Financeira: Controle de contas a pagar/receber, orçamento e relatórios financeiros.
+    - Controle de Produção e PCP (Planejamento e Controle da Produção): Gerenciamento do andamento de ordens de produção e
+    controle de processos.
+    - Integração com CRMs: Ferramentas para organizar e gerenciar o relacionamento com clientes, fornecedores, etc.
+    - Controle de Ponto: Gerenciamento das jornadas de trabalho da equipe, incluindo controle de horas trabalhadas.
+Segmentos atendidos:
+    - Prestadores de Serviços: Empresas que oferecem diversos tipos de serviços e precisam gerenciar oportunidades de negócios,
+    contratos, ordens de serviço, etc.
+    - Comércios: Empresas que vendem produtos diretamente e precisam gerenciar estoques, vendas, processos de pagamento e
+    fechamento de caixa.
+    - Indústrias: Empresas que produzem ou fazem a transformação de produtos, necessitando de controle rígido das compras de
+    matérias-primas e gestão de produção.
+Diferenciais:
+    - Suporte em tempo real para resolver questões técnicas ou operacionais.
+    - Equipe especializada com mais de 12 anos de experiência.
+    - Compromisso com resultados, focando no cumprimento de prazos e na melhoria contínua do desempenho do negócio.
+
+2. WhatsApp e Comunicadores
+Integrar diversos canais de comunicação, como WhatsApp, em uma plataforma única, melhorando a agilidade e organização no
+atendimento ao cliente.
+Como Funciona:
+    - Centralização de Canais: Todos os contatos dos principais apps de comunicação (WhatsApp, chat online, etc.) são
+    centralizados em uma única plataforma.
+    - Automação de Atendimento: Utilização de robôs de atendimento para automatizar a triagem e qualificações de leads e mensagens.
+    - Gerenciamento de Equipe e Fluxo de Trabalho: Facilitação da organização do atendimento, com possibilidade de transferir
+    mensagens para o departamento ou atendente correto com apenas um clique.
+    - Monitoramento de Atendimentos: Supervisão em tempo real dos atendimentos e interações entre os colaboradores e os clientes.
+    - Home Office: Controle dos atendimentos e monitoramento do desempenho da equipe, mesmo que os colaboradores estejam em
+    home office.
+Principais Funcionalidades do Serviço de WhatsApp e Comunicadores:
+    - Painel Gerencial: Visualização em tempo real de todos os atendimentos realizados, performance da equipe e resultados obtidos.
+    - Robôs de Atendimento: Triagem e organização automatizada dos contatos. Os robôs realizam o atendimento inicial e passam o
+    caso para os operadores apenas quando necessário.
+    - Integração com CRM: Melhora a experiência do cliente e otimiza o processo de cadastro e acompanhamento de informações.
+    - Gestão de Atendimento 24/7: Os robôs podem atender fora do horário comercial, garantindo que nenhuma dúvida ou problema
+    seja deixado sem resposta.
+    - Organização por Departamentos e Equipe: Direcionamento das mensagens automaticamente para os atendentes ou departamentos
+    corretos.
+    - Monitoramento de Home Office: Ferramentas específicas para empresas com equipes trabalhando remotamente, permitindo o controle
+    e supervisão em tempo real.
+Diferenciais:
+    - Atendimento Ágil e Organizado: Organiza o atendimento de forma eficiente, sem deixar contatos sem resposta.
+    - Suporte em Tempo Real: Equipe de suporte disponível para resolver qualquer problema técnico.
+    - Integração com CRMs e ferramentas de gestão para maior agilidade e performance.
+    - Personalização de Atendimento: Configuração de automação e scripts de atendimento de acordo com as necessidades da empresa.
+
+3. Websites
+Criar sites institucionais, e-commerces, landing pages e blogs com design moderno, responsivo e otimizado para conversão, para
+empresas de diferentes portes e nichos.
+Como Funciona:
+    - Planejamento e Análise de Necessidades: Compreensão do objetivo do site, da identidade da empresa e do público-alvo.
+    - Desenvolvimento Personalizado: Criação de sites personalizados de acordo com a estratégia de marketing e objetivos da empresa.
+    - Design Responsivo: Desenvolvimento de sites que se ajustam perfeitamente a qualquer dispositivo, como smartphones,
+    tablets e desktops.
+    - SEO e Otimização: Sites otimizados para motores de busca (SEO), garantindo maior visibilidade nas pesquisas e atração de
+    visitantes qualificados.
+    - Lançamento e Acompanhamento: Após a entrega do site, acompanhamento do desempenho e melhorias contínuas para aumentar a
+    eficácia e conversão.
+Principais Funcionalidades dos Websites:
+    - Design Moderno e Responsivo: Layouts atraentes e responsivos que se ajustam a diferentes dispositivos.
+    - Integração com Redes Sociais: Conexão com plataformas como Facebook, Instagram, entre outras, para facilitar o
+    compartilhamento e engajamento.
+    - Otimização para SEO: Ferramentas de SEO integradas para garantir visibilidade nos motores de busca.
+    - Gerenciamento Simples: Interface de administração amigável, permitindo que a empresa atualize o conteúdo do site de forma
+    simples.
+    - Análise de Desempenho: Relatórios e métricas de desempenho, incluindo visitas, taxas de conversão e comportamentos do usuário.
+Diferenciais:
+    - Design Personalizado: Desenvolvimento de sites únicos, alinhados com a identidade visual e as necessidades da empresa.
+    - Otimização para Conversão: Sites focados em conversão, com estratégias de UX (User Experience) para melhorar a jornada
+    do usuário.
+    - Suporte Técnico: Equipe especializada para prestar suporte contínuo e resolver qualquer questão técnica.
+
+4: Website Rápido
+Criar websites ágeis e com baixo custo, com foco em resultados rápidos, para empresas que precisam de presença online imediata,
+sem perder a qualidade e performance.
+Como Funciona:
+    - Criação Rápida de Layouts Pré-definidos: Utilização de templates prontos e personalizáveis para acelerar o processo de
+    desenvolvimento.
+    - Foco na Velocidade de Carregamento: Sites otimizados para carregamento rápido e boa experiência do usuário.
+    - Solução Custo-Benefício: Criamos um site eficiente com baixo custo e alto retorno, focado na funcionalidade básica e objetiva.
+    - Lançamento Rápido: Entrega em tempo recorde, com um site pronto para ser lançado e gerenciar de forma eficaz a presença online.
+Principais Funcionalidades do Website Rápido:
+    - Templates Prontos: Escolha entre uma série de templates otimizados que atendem diferentes nichos de mercado.
+    - Integração Básica com Redes Sociais: Links para redes sociais e outras plataformas, melhorando a interação com o público.
+    - SEO Básico: Otimização para motores de busca para garantir que o site seja encontrado pelos clientes.
+    - Fácil Atualização: Plataforma simples de administrar, permitindo que a empresa atualize seu conteúdo com facilidade.
+    - Suporte Inicial: Suporte técnico imediato durante a fase de implantação.
+Diferenciais:
+    - Entrega Rápida: O site é desenvolvido e entregue em um prazo curto, ideal para quem precisa de presença online imediata.
+    - Custo Benefício: Solução econômica para empresas que buscam um site simples, eficiente e de boa performance.
+
+Informações solicitadas que devem ser tratadas como links em markdown:
+- Contato Suporte [(15) 3042-0727](tel:+551530420727)
+- Contato Comercial: ([15) 99604-4772](https://api.whatsapp.com/send?phone=5515996044772)
+- Contato de E-mail: [contato@beforce.com.br](mailto:contato@beforce.com.br)
+- Link do Website: [Beforce Website](https://www.beforce.com.br)
+- Endereço: Rua Santa Clara, 199, 2º Andar, Centro, Sorocaba, SP, CEP: 18035-251
+`
